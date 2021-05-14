56f7675573745e1efa2a456aecb1646c5f8d00ae
Igor Tretiakov <itretiakov@sphereinc.com>
2021-05-14 10:35:58 +0300

    Init

---
+++ lib/refund_protect.rb
@@ -0,0 +1,6 @@
+require "refund_protect/version"
+
+module RefundProtect
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/refund_protect/version.rb
@@ -0,0 +1,3 @@
+module RefundProtect
+  VERSION = "0.1.0"
+end
